e99f2be77009b51080f6b25d6182acd425f703fd
amarbajric <amar.bajric78@gmail.com>
2017-01-19 22:44:58 +0100

    added search parameters instead of hardcoded url

--- src/app/services/rentedCar.service.ts
+++ src/app/services/rentedCar.service.ts
@@ -3,6 +3,7 @@ import {Injectable, Inject} from "@angular/core";
 import {Http, URLSearchParams, Headers} from "@angular/http";
 import {BASE_URL_RENTEDCARS} from "../app.tokens";
 import {Observable} from "rxjs";
+import {error} from "util";
 
 @Injectable()
 export class RentedCarService{
@@ -19,16 +20,17 @@ export class RentedCarService{
 
 
   getId(id:number): void {
-    let url = this.baseUrl + "/search/findRentedCar?customer=" + sessionStorage.getItem("customerID") + "&car=" + id;
-
-    console.log("Input id: " + id);
+    let url = this.baseUrl + "/search/findRentedCar";
+    let search = new URLSearchParams();
+    search.set('customer', sessionStorage.getItem("customerID"));
+    search.set('car', id.toString());
 
     let headers = new Headers();
     headers.set('Accept', 'application/json');
 
     this
       .http
-      .get(url, {headers})
+      .get(url, {headers, search})
       .map(resp => resp.json())
       .subscribe(id => {
         this.rentedCarId = id;
@@ -40,14 +42,15 @@ export class RentedCarService{
   }
 
 
-
   deleteRentedCarEntry(rowID:number): void {
     let url = this.baseUrl+"/"+rowID;
 
     this.http
       .delete(url)
       .map(resp => resp.json())
-      .subscribe(res => res)
+      .subscribe(res => res,
+                 error => console.log(error)
+      )
 }
 
 
